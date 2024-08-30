b9bbba35fb0ed646c9ddb4001ed38c0d5a76fce4
Cihan Hasanoglu <cihan.hasanoglu@icloud.com>
2024-08-30 12:55:24 +0200

    Initial Commit

---
+++ QAChallengeAutomation/QAChallengeAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QAChallengeAutomationApp.swift
+//  QAChallengeAutomation
+//
+//  Created by Hasanoglu, Cihan on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct QAChallengeAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
